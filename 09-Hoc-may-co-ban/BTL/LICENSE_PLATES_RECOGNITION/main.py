0ab5604fd72b3ec23b34a259ef6609b2a24cd40f
NTQ-SOLUTION\duong.nguyen14 <duong.nguyen14@ntq-solution.com.vn>
2023-11-01 20:05:24 +0700

    Upload 09 end of day

---
+++ 09-Hoc-may-co-ban/BTL/LICENSE_PLATES_RECOGNITION/main.py
@@ -0,0 +1,10 @@
+from LNRDetector import LNRDetector
+from KNNDetector import KNNDetector
+from LGRDetector import LGRDetector
+
+
+if __name__ == '__main__':
+    lnr = LNRDetector()
+    knn = KNNDetector()
+    lgr = LGRDetector()
+    pass
