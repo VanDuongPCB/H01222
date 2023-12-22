c956aca04a27050a95459e0eed803eb261717dfb
NTQ-SOLUTION\duong.nguyen14 <duong.nguyen14@ntq-solution.com.vn>
2023-12-22 13:17:46 +0700

    Remove

--- 09-Hoc-may-co-ban/BTL/LICENSE_PLATES_RECOGNITION/03-character-probability.py
+++
@@ -1,8 +0,0 @@
-import os 
-import numpy as np
-
-path = 'character_images/blackwhites'
-list_dir = os.listdir(path)
-for dir in list_dir:
-    list_file = os.listdir(path + '/' + dir)
-    print(dir, ',', len(list_file))
